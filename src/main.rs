965ae827bcc1535310b1971ebabe1586307f1120
BaconWaffleDonut <baconwaffledonut@proton.me>
2025-06-18 21:04:43 -0700

    allows user to select from one of the options

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-
+use inquire::{InquireError, Select};
 fn main() {
     println!("Welcome to my totally awesome let's play! On todays episode we will be committing tax fraud! /j");
 
-    println!("Please select what to do!");
-
-}
+    let v1i: Vec<&str>  = vec!["Add", "Withdraw", "Check"];
+    let v1o: Result<&str, InquireError> = Select::new("Please select what you want to do!", v1i).prompt();
+    match v1o {
+        Ok(choice) => println!("Transferring you to {choice}"),
+        Err(_) => println!("There was an error, please try again."),
+    }
+}   
