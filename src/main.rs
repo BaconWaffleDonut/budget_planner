f94c18592f180d90f63bb75e4876f1c628853b86
BaconWaffleDonut <baconwaffledonut@proton.me>
2025-06-19 20:59:06 -0700

    removed input, changed readme

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use inquire::{InquireError, Select};
+
 fn main() {
     println!("Welcome to my totally awesome let's play! On todays episode we will be committing tax fraud! /j");
 
     let v1i: Vec<&str>  = vec!["Add", "Withdraw", "Check"];
     let v1o: Result<&str, InquireError> = Select::new("Please select what you want to do!", v1i).prompt();
-    match v1o {
-        Ok(choice) => println!("Transferring you to {choice}"),
-        Err(_) => println!("There was an error, please try again."),
-    }
+    //find some way to remove inquireerror from v1o PLEASE FUTURE ME, in like two minutes that is....... mjaybe i just find other way to handle input 
+
+    //println!("test: {choice}");
 }   
